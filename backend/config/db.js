46ffa17f8daf69b95dfe1707771c4976f5ff0606
Simmi Verma <svindbhopal@gmail.com>
2026-07-29 10:39:37 +0530

    Support MONGO_URL and MONGO_URI in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/skillnest');
+    const dbUri = process.env.MONGO_URI || process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/skillnest';
+    const conn = await mongoose.connect(dbUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Database Connection Error: ${error.message}`);
